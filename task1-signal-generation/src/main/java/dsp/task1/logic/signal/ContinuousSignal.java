b885509e32fabaa5d4825872f1a9b2637aacd4e6
irek bartoszek <bartoszir@gmail.com>
2026-05-28 22:21:43 +0200

    refactor: remove old task1-signal-generation module

--- task1-signal-generation/src/main/java/dsp/task1/logic/signal/ContinuousSignal.java
+++
@@ -1,9 +0,0 @@
-package dsp.task1.logic.signal;
-
-public abstract class ContinuousSignal extends Signal {
-    public ContinuousSignal(double amplitude, double startTime, double duration) {
-        super(amplitude, startTime, duration);
-    }
-
-    public abstract double getValueAt(double t);
-}
--- task1-signal-generation/src/main/java/dsp/task1/logic/signal/DiscreteSignal.java
+++
@@ -1,9 +0,0 @@
-package dsp.task1.logic.signal;
-
-public abstract class DiscreteSignal extends Signal {
-    public DiscreteSignal(double amplitude, double startTime, double duration) {
-        super(amplitude, startTime, duration);
-    }
-
-    public abstract double getValueAtSample(int n);
-}
